260f2cabb3aa59b4426ecc624aedf356b2b785e2
Mohamed Mansour <hello@mohamedmansour.com>
2016-03-05 17:56:25 -0800

    Skin home page

--- public/javascripts/controllers/video-list-controller.js
+++ public/javascripts/controllers/video-list-controller.js
@@ -1,3 +1,24 @@
 App.controller('VideoListController', ['$scope', function($scope) {
     $scope.foo = 'bar';
+    $scope.banner = '/images/banners/warcraft.png';
+    $scope.videos = [
+        {
+            title: 'Warcraft (2016)',
+            frames: [
+                {
+                    img: '',
+                    timestamp: '1:20'
+                }
+            ]
+        },
+        {
+            title: 'Mr Robot (2015)',
+            frames: [
+                {
+                    img: '',
+                    timestamp: '1:20'
+                }
+            ]
+        }
+    ];
 }]);
