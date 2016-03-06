7ebe9691baa8d393d43c57c0109e4dd1698c781d
Mohamed Mansour <hello@mohamedmansour.com>
2016-03-06 08:01:38 -0800

    stylize the meme pages and list

--- public/javascripts/controllers/meme-controller.js
+++ public/javascripts/controllers/meme-controller.js
@@ -1,3 +1,5 @@
-App.controller('MemeController', ['$scope',  function($scope) {
-
+App.controller('MemeController', ['$scope', '$http', '$routeParams',  function($scope, $http, $routeParams) {
+    $http.get('/api/meme/' + $routeParams.id).success(function(data) {
+        $scope.meme = data;
+    });
 }]);
--- public/javascripts/controllers/meme-list-controller.js
+++ public/javascripts/controllers/meme-list-controller.js
@@ -1,5 +1,5 @@
 App.controller('MemeListController', ['$scope', '$http',  function($scope, $http) {
-    $http.get('/api/memes').success(function(data) {
-        $scope.videos = data;
+    $http.get('/api/meme').success(function(data) {
+        $scope.memes = data;
     });
 }]);
