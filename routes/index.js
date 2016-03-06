e6b0057d2dc0b02264876006397c2a413b5b4545
Mohamed Mansour <hello@mohamedmansour.com>
2016-03-06 11:08:44 -0800

    update twitter metadata

--- routes/index.js
+++ routes/index.js
@@ -10,12 +10,24 @@ module.exports = function(models) {
     });
 
     router.get('/memes/:id', function(req, res, next) {
+        var hostUrl = req.protocol + '://' + req.get('host');
+        var fullUrl = hostUrl + req.originalUrl;
+        console.log(fullUrl);
         if (process.env.NODE_ENV == 'dev') {
-             res.render('memepage', {"_id":"56dc3cd6a6dcec520fd5e564","UserId":"1","VideoTitle":"warcraft","MemeUrl":"/api/image/meme__created_1457274070338__vid_warcraft.png","__v":0});
+            var meme =  {"_id":"56dc3cd6a6dcec520fd5e564","UserId":"1","VideoTitle":"warcraft","MemeUrl":"/api/image/meme__created_1457274070338__vid_warcraft.png","__v":0};
+             res.render('memepage', {
+                fullUrl: fullUrl,
+                imageUrl: hostUrl + meme.MemeUrl,
+                meme: meme
+             });
             return;
         }
         models.Memes.findOne({_id: req.params.id}, function(err, meme){
-            res.render('memepage', meme);
+            res.render('memepage', {
+                fullUrl: fullUrl,
+                imageUrl: hostUrl + meme.MemeUrl,
+                meme: meme
+            });
         });
     });
 
