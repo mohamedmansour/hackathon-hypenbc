14cd07c65402076241e3200723bd0b21dcd7f2b1
Mohamed Mansour <hello@mohamedmansour.com>
2016-03-05 21:50:17 -0800

    Add Videos Mock

--- routes/api.js
+++ routes/api.js
@@ -15,7 +15,52 @@ db.auth(function(error) {
     }
 });
 
-module.exports = function(io) {  
+module.exports = function(io) { 
+  router.get('/videos', function(req, res, next) {
+    res.send([
+        {
+            title: 'Warcraft (2016)',
+            bannerImage: '/images/banners/warcraft.png',
+            runtime: 7200,
+            hypes: [
+                {
+                    thumbnail: '/images/frames/frame1.png',
+                    timestamp: '1:20',
+                    timemark: '1:20'
+                },
+                {
+                    thumbnail: '/images/frames/frame3.png',
+                    timestamp: '3:20',
+                    timemark: '3:20'
+                }
+            ]
+        },
+        {
+            title: 'Mr Robot (2015)',
+            subtitle: 'Season 1, Episode 5',
+            bannerImage: '/images/banners/warcraft.png',
+            runtime: 3600,
+            hypes: [
+                {
+                    thumbnail: '/images/frames/frame1.png',
+                    timestamp: '1:20',
+                    timemark: '1:20'
+                },
+                {
+                    thumbnail: '/images/frames/frame2.png',
+                    timestamp: '5:20',
+                    timemark: '5:20'
+                },
+                {
+                    thumbnail: '/images/frames/frame3.png',
+                    timestamp: '10:20',
+                    timemark: '10:20'
+                }
+            ]
+        }
+    ]);    
+  });
+  
   /* GET users listing. */
   router.get('/hype', function(req, res, next) {
     var connectedClients = io.sockets.clients().connected;
