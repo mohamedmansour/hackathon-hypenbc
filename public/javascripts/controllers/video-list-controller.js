a4a0109c54a2345ee9b913f6d679d3f79a9259f6
Mohamed Mansour <hello@mohamedmansour.com>
2016-03-05 22:27:22 -0800

    Add frames UX

--- public/javascripts/controllers/video-list-controller.js
+++ public/javascripts/controllers/video-list-controller.js
@@ -1,22 +1,43 @@
 App.controller('VideoListController', ['$scope', function($scope) {
     $scope.foo = 'bar';
-    $scope.banner = '/images/banners/warcraft.png';
     $scope.videos = [
         {
-            title: 'Warcraft (2016)',
-            frames: [
+            title: "Warcraft (2016)",
+            bannerImage: "/images/banners/warcraft.png",
+            runtime: 7200,
+            hypes: [
                 {
-                    img: '',
-                    timestamp: '1:20'
+                    thumbnail: '/images/frames/frame1.png',
+                    timestamp: '1:20',
+                    timemark: '1:20'
+                },
+                {
+                    thumbnail: '/images/frames/frame3.png',
+                    timestamp: '3:20',
+                    timemark: '3:20'
                 }
             ]
         },
         {
-            title: 'Mr Robot (2015)',
-            frames: [
+            title: "Mr Robot (2015)",
+            subtitle: "Season 1, Episode 5",
+            bannerImage: "/images/banners/warcraft.png",
+            runtime: 3600,
+            hypes: [
                 {
-                    img: '',
-                    timestamp: '1:20'
+                    thumbnail: '/images/frames/frame1.png',
+                    timestamp: '1:20',
+                    timemark: '1:20'
+                },
+                {
+                    thumbnail: '/images/frames/frame2.png',
+                    timestamp: '5:20',
+                    timemark: '5:20'
+                },
+                {
+                    thumbnail: '/images/frames/frame3.png',
+                    timestamp: '10:20',
+                    timemark: '10:20'
                 }
             ]
         }
